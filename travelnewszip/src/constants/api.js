34906a5f03f059e9936d30263309d928048768f0
FranklinAlf <76907420+FranklinAlf@users.noreply.github.com>
2021-01-17 21:06:38 +0100

    Add files via upload

---
+++ travelnewszip/src/constants/api.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://wp-subdir.TravelNews.com/wp-json/wp/v2/posts";
+export const ISLANDS_URL = BASE_URL + "?categories=2";
+export const COUNTRIES_URL = BASE_URL + "?categories=3";
+export const CRUISES_URL = BASE_URL + "?categories=4";
+
+
